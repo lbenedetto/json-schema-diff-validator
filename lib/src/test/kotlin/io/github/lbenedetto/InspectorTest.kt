46eef3d5a2f85f954ae53f369e393c505075e158
Bruno <178160780+bfreitastgtg@users.noreply.github.com>
2025-08-26 16:29:00 +0200

    fix-nullable-type-change (#15)

--- lib/src/test/kotlin/io/github/lbenedetto/InspectorTest.kt
+++ lib/src/test/kotlin/io/github/lbenedetto/InspectorTest.kt
@@ -1,14 +1,9 @@
 package io.github.lbenedetto
 
 import io.github.lbenedetto.inspector.DetectedChanges
-import io.github.lbenedetto.inspector.FieldTypeChange
 import io.github.lbenedetto.inspector.Inspector
-import io.github.lbenedetto.util.PatchDSL.jsonString
-import io.github.lbenedetto.util.PatchDSL.replace
 import io.github.lbenedetto.util.Util
-import io.github.lbenedetto.util.Util.withPatches
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 
 internal class InspectorTest : BehaviorSpec({
@@ -21,19 +16,6 @@ internal class InspectorTest : BehaviorSpec({
 
       Then("Change should be detected") {
         Inspector.inspect(oldSchema, newSchema) shouldBe DetectedChanges()
-      }
-    }
-
-    When("A field type is changed") {
-      val oldSchema = Util.readSchema("ExampleObject.schema")
-      val newSchema = oldSchema.withPatches(
-        replace("/properties/someIntegerField/type", jsonString("string"))
-      )
-
-      Then("Change should be detected") {
-        Inspector.inspect(oldSchema, newSchema).all().shouldContainExactlyInAnyOrder(
-          FieldTypeChange("/properties/someIntegerField/type", "[INTEGER]", "[STRING]")
-        )
       }
     }
   }
