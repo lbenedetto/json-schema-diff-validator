10d394392c9a1080d54de5a04d8b6df9c44616db
Lars Benedetto <4466272+lbenedetto@users.noreply.github.com>
2025-08-26 05:37:03 -0700

    Fix typo in NotAbsentRequirementChange toString (#14)

--- lib/src/test/kotlin/io/github/lbenedetto/MultipleChangesTest.kt
+++ lib/src/test/kotlin/io/github/lbenedetto/MultipleChangesTest.kt
@@ -22,14 +22,14 @@ class MultipleChangesTest : BehaviorSpec({
                     "FieldChange: ADDED field optionalProp at /properties as nullable}",
                     "FieldChange: ADDED field optionalProp at /properties as optional}",
                     "NonNullRequirementChange: REMOVED non-null requirement for value at /properties",
-                    "NotAbsentRequirementChange: REMOVED non-null requirement for value at /properties"
+                    "NotAbsentRequirementChange: REMOVED not-absent requirement for value at /properties"
                 )
                 validationResult.risky shouldBe emptyList()
                 validationResult.fatal.shouldContainExactlyInAnyOrder(
                     "FieldChange: ADDED field reqProp at /properties as non-null}",
                     "FieldChange: ADDED field reqProp at /properties as required}",
                     "NonNullRequirementChange: ADDED non-null requirement for reqProp at /properties",
-                    "NotAbsentRequirementChange: ADDED non-null requirement for reqProp at /properties"
+                    "NotAbsentRequirementChange: ADDED not-absent requirement for reqProp at /properties"
                 )
             }
         }
