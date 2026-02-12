350ddec5d7b9b6c2a4220f1dc443dc658b5fc1e5
Bruno <178160780+bfreitastgtg@users.noreply.github.com>
2026-02-12 14:20:36 +0100

    downgrade java version (#29)
    * downgrade java version
    
    * update ci
    
    * add missing permission
    
    * sync with .idea
    
    ---------
    
    Co-authored-by: larsbenedetto <lbenedetto@toogoodtogo.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -42,7 +42,7 @@ java {
   withSourcesJar()
 
   toolchain {
-    languageVersion = JavaLanguageVersion.of(25)
+    languageVersion = JavaLanguageVersion.of(24)
   }
 }
 
@@ -54,7 +54,7 @@ tasks.named<Jar>("javadocJar") {
 
 object ProjectInfo {
   const val GROUP = "io.github.lbenedetto"
-  const val VERSION = "1.0.2"
+  const val VERSION = "1.0.3"
 }
 
 group = ProjectInfo.GROUP
